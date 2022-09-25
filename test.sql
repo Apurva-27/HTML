a224434684ba0bd81663a5386ba51a995f5ef05e
Apurva Sonawane <62685677+Apurva-27@users.noreply.github.com>
2022-09-25 14:38:30 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS test223 (           
+  message varchar(255) NOT NULL         
+  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;         
+  INSERT INTO test223(message) VALUES('Ansible To Do List');
+  INSERT INTO test223(message) VALUES('Get ready');         
+  INSERT INTO test223(message) VALUES('Ansible is fun')
